92e2ff7cc72038dc912555cfeb704a08662712de
YoussefAbdelnaser <youssefabdelnasersobhy@yahoo.com>
2023-07-04 07:54:17 +0300

    Second Commit

--- client/src/components/FormFeild.jsx
+++ client/src/components/FormFeild.jsx
@@ -37,6 +37,7 @@ const FormFeild = ({
         value={value}
         onChange={handleChange}
         required
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] outline-none block w-full p-3"
       />
     </div>
   );
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -14,6 +14,7 @@ const CreatePost = () => {
   const handleSubmit = () => {};
   const handleChange = (e) => {};
   const handleSurpriseMe = () => {};
+  const generateImage = () => {};
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -43,6 +44,46 @@ const CreatePost = () => {
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-63 flex justify-center items-center">
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/1 h-9/12 object-contain opacity-40"
+              />
+            )}
+            {generatingImg && (
+              <div className="absolute  inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={generateImage}
+            className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {generatingImg ? "Generating Image!" : "Generate"}
+          </button>
+        </div>
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">
+            You can share the image after its been created
+          </p>
+          <button
+            type="submit"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {loading ? "Sharing..." : "Share"}
+          </button>
         </div>
       </form>
     </section>
